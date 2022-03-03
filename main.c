1fcf8e42b7062096abb4663803f67cf2562b6b55
Mateusz101 <100830652+Mateusz101@users.noreply.github.com>
2022-03-03 08:25:29 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define pi 3.14
+int main(void) {
+  int r = 10; // kom 
+  double pole = pi * r * r; 
+printf("Pole jest równe %f\n",pole);
+  
+}
+/* funkcja scan i przetestowac wprowadzenie danych do programu i zrobic dwa pierwsze zadania i dokończyć aktywachje konta na githubie*/
